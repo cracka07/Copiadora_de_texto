103291e88746e4ef0098a95095d3a37cb43ac6f5
mariano <mariano_fferrer@hotmail.com>
2023-07-03 00:56:46 -0300

    first commit

---
+++ src/logica/Copiadora.java
@@ -0,0 +1,15 @@
+
+package logica;
+
+import igu.Pantalla;
+
+
+public class Copiadora {
+    
+    
+     public static void main(String[] args) {
+         Pantalla pant=new Pantalla();
+         pant.setVisible(true);
+         pant.setLocationRelativeTo(null);
+    }
+}
